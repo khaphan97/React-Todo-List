a8650208229fb8fe9f2a7a63dffe6bc3a95c3409
phanhuykha123 <66725049+phanhuykha123@users.noreply.github.com>
2021-05-12 20:36:20 +0700

    update build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-        <div div className="page-header" >
+        <div className="page-header" >
             <h1>Project Todolist <small>ReactJs</small></h1>
         </div>
     );
